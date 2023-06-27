b7f263af52a16c2bacc10e5047c4a2eb6c0dcc80
Joshua Graham <Joshua.Graham@softwire.com>
2023-06-27 13:51:21 +0100

    Part2c

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,16 @@
 // This is our main function
 function fizzbuzz() {
     let s=""
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= 200; i++) {
         s=""
+
+        if (isDiv(i,11)) {
+            s="Bong";
+            s=isFezz(i, s);
+            console.log(s);
+            continue;
+        }
+
         if (isDiv(i,3)) {
             s+="Fizz";
         }
@@ -13,9 +21,9 @@ function fizzbuzz() {
             s+="Bang";
         }
 
-        if (isDiv(i,11)) {
-            console.log("Bong");
-        } else if (isDiv(i,3) || isDiv(i,5) || isDiv(i,7)) {
+        s=isFezz(i, s);
+
+        if (isDiv(i,3) || isDiv(i,5) || isDiv(i,7) || isDiv(i, 13)) {
             console.log(s);
         } else {
             console.log(i);
@@ -24,9 +32,19 @@ function fizzbuzz() {
 }
 
 function isDiv(x,y) {
-    return (x%y === 0) ? true : false
+    return (x % y === 0)
+}
+
+function isFezz(i, s) {
+    if (isDiv(i,13)) {
+        let index = s.indexOf("B");
+        if (index === -1) {
+            index=0
+        }
+        s = s.slice(0, index) + "Fezz" + s.slice(index);
+    }
+    return s
 }
 
 // Now, we run the main function:
 fizzbuzz();
-
